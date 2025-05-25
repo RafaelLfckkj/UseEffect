cfe007426bb571920e129d29f5d5a488d9ca3078
rafaxolive <rafaxolive@gmail.com>
2025-05-25 01:57:42 +0000

    Segundo Video finalizado

--- src/components/Exemplo2.jsx
+++ src/components/Exemplo2.jsx
@@ -1,11 +1,24 @@
 import { useState, useEffect } from "react";
 
+import axios from "axios";
+
 export function Exemplo2() {
   const [cep, setCep] = useState("");
+  const [logradouro, setLougradouro] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [localidade, setLocalidade] = useState("");
+  const [estado, setEstado] = useState("");
+  const [uf, setUf] = useState("");
 
   useEffect(() => {
-    if(cep.length === 8){
-      console.log(cep)
+    if (cep.length === 8) {
+      axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
+        setLougradouro(response.data.logradouro);
+        setBairro(response.data.bairro);
+        setLocalidade(response.data.localidade);
+        setEstado(response.data.estado);
+        setUf(response.data.uf);
+      });
     }
   }, [cep]);
 
@@ -19,6 +32,19 @@ export function Exemplo2() {
           placeholder="Digite o CEP"
           onChange={(input) => setCep(input.target.value)}
         />
+
+        {cep.length === 8 && (
+          <>
+          <h3>Suas informações:</h3>
+          <ul>
+            <li>Logradouro: {logradouro}</li>
+            <li>Bairro: {bairro}</li>
+            <li>Cidade: {localidade}</li>
+            <li>Estado: {estado}</li>
+            <li>UF: {uf}</li>
+          </ul>
+          </>
+        )}
       </div>
     </section>
   );
